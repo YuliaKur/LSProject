40a10aad345d09e212e6885c716a8a8d7d6435b5
YuliaKur <yulkur.reignsupreme@gmail.com>
2022-10-25 00:45:00 +0300

    Lesoon15
    доделывали кнопки, начала делать Header

--- dev/assets/js/global.js
+++ dev/assets/js/global.js
@@ -36,4 +36,29 @@ jQuery(document).ready(function($) {
     if($('.post-item iframe').length){
         $('.post-item iframe').closest('p').addClass('iframe-box')
     }
+
+    var nav = $('.language-title');
+    var selection = $('.language-list');
+    var select = selection.find('li');
+
+    nav.click(function(event) {
+        if (nav.hasClass('active')) {
+            nav.removeClass('active');
+            selection.stop().slideUp(200);
+        } else {
+            nav.addClass('active');
+            selection.stop().slideDown(200);
+        }
+        event.preventDefault();
+    });
+
+    select.click(function(event) {
+        select.removeClass('active');
+        $(this).addClass('active');
+        var $lang = $(this).text();
+        nav.text($lang);
+        nav.trigger('click');
+        //alert($lang);
+        // alert ("location.href = 'index.php?lang=" + $(this).attr('data-value'));
+    });
 })
